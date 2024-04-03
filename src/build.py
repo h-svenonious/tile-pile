b9cd400559d2045891b5fa9a8ffa52a4e423900a
Hermann Svenonious <hermann.svenonious@posteo.de>
2024-04-03 12:56:17 +0200

    fix path in build script

--- src/build.py
+++ src/build.py
@@ -1,6 +1,7 @@
 '''
 Tiles generator
 '''
+import sys
 
 TILES_FILE_TEMPLATE = """<!DOCTYPE html>
 <html>
@@ -33,6 +34,7 @@ INDEX_LINKS_TEMPLATE = """
     </div>
 """
 
+web_dir = sys.argv[1]
 
 # create pairs (N, op)
 links = ""
@@ -40,9 +42,9 @@ for xx, nn in enumerate(range(10, 400, 80)):
     for yy, op in enumerate(range(1, 10, 2)):
         #print(f'({nn}, {op / 10})', end='')
         filename = f'tiles_{nn}_{op}.html'
-        with open('web/' + filename, 'w') as _f:
+        with open(web_dir + filename, 'w') as _f:
             _f.write(TILES_FILE_TEMPLATE.format(nn, op / 10))
         links += INDEX_LINKS_TEMPLATE.format(xx * 100 + 10, yy * 100 + 10, filename, filename)
 
-with open('web/index.html', 'w') as _f:
+with open(web_dir + 'index.html', 'w') as _f:
     _f.write(INDEX_TEMPLATE.format(links))
